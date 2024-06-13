6aafdc2a5d599f2febf4af355516c20abbc3f4ca
MMitsuha <miyammitsuha@gmail.com>
2024-06-13 13:53:16 +0800

    Add args

--- shadow-server/src/lib.rs
+++ shadow-server/src/lib.rs
@@ -1,3 +1,22 @@
 mod misc;
 pub mod network;
 pub mod web;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    about,
+    long_about = "A high performance rat server and client written in 100% safe rust"
+)]
+pub struct AppArgs {
+    #[arg(short, long, default_value_t = String::from("debug"))]
+    pub verbose: String,
+
+    #[arg(short, long, default_value_t = String::from("0.0.0.0:1244"))]
+    pub server_addr: String,
+
+    #[arg(short, long, default_value_t = String::from("0.0.0.0:9000"))]
+    pub web_addr: String,
+}
--- shadow-server/src/main.rs
+++ shadow-server/src/main.rs
@@ -1,21 +1,24 @@
 use anyhow::Result as AppResult;
+use clap::Parser;
 use flexi_logger::Logger;
-use shadow_server::{network, web};
+use shadow_server::{network, web, AppArgs};
 use std::{collections::HashMap, sync::Arc};
 use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() -> AppResult<()> {
+    let args = AppArgs::parse();
+
     // Start logging
     #[cfg(debug_assertions)]
-    Logger::try_with_str("debug")?.start()?;
+    Logger::try_with_str(args.verbose)?.start()?;
 
     // A instance representing all clients connected to the server
     let server_objs = Arc::new(RwLock::new(HashMap::new()));
     // Server config
-    let server_cfg = network::Config::new("0.0.0.0:1244".parse()?);
+    let server_cfg = network::Config::new(args.server_addr.parse()?);
     // Web interface config
-    let web_cfg = web::Config::new("0.0.0.0:9000".parse()?);
+    let web_cfg = web::Config::new(args.web_addr.parse()?);
 
     // Start the server
     let server = tokio::spawn(network::run(server_cfg, server_objs.clone()));
