1390fbc2ea1d0cfc64b6fa81890c8dcc4ec98238
MMitsuha <miyammitsuha@gmail.com>
2024-06-14 19:20:19 +0800

    Add argument

--- shadow-client/src/lib.rs
+++ shadow-client/src/lib.rs
@@ -1,2 +1,19 @@
 mod misc;
 pub mod network;
+
+use clap::Parser;
+
+/// Server command line arguments
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    about,
+    long_about = "A high performance rat server and client written in 100% safe rust"
+)]
+pub struct AppArgs {
+    #[arg(short, long, default_value_t = String::from("debug"))]
+    pub verbose: String,
+
+    #[arg(short, long, default_value_t = String::from("127.0.0.1:1244"))]
+    pub server_addr: String,
+}
--- shadow-client/src/main.rs
+++ shadow-client/src/main.rs
@@ -1,16 +1,20 @@
 #![windows_subsystem = "windows"]
 
 use anyhow::Result as AppResult;
+use clap::Parser;
 use flexi_logger::Logger;
-use shadow_client::network;
+use shadow_client::{network, AppArgs};
 
 #[tokio::main]
 async fn main() -> AppResult<()> {
+    // Parse arguments
+    let args = AppArgs::parse();
+
     #[cfg(debug_assertions)]
-    Logger::try_with_str("debug")?.start()?;
+    Logger::try_with_str(args.verbose)?.start()?;
 
     // Server config
-    let server_cfg = network::Config::new("127.0.0.1:1244".parse()?);
+    let server_cfg = network::Config::new(args.server_addr.parse()?);
     network::run(server_cfg).await?;
 
     Ok(())
